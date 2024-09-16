0af7380107630f511ed0c1469f55aedead9f7dc1
Domingos Ferreira <dfadomingos47@gmail.com>
2024-09-16 20:36:09 -0300

    .

--- Lista_exercicios_3/exe10-1.js
+++ Lista_exercicios_3/exe10-1.js
@@ -6,22 +6,20 @@ let soma_par = 0
 for(let i = 1; i<=10; i++){
     let num = Number(prompt(`Digite um número: `))     
 
+    let cont_div = 0
+    for(let divisor = num; divisor >= 1; divisor--){        
+        if(num % divisor == 0){
+            cont_div++
+        }
+    }
+    if(cont_div <= 2){        
+        soma_primo = soma_primo + num
+    }
+
     if(num % 2 == 0){
         soma_par = soma_par + num
     }
-
-    let primo = true
-    let divisor = 2
-    while(divisor < num){
-        if(num % divisor == 0){
-            primo = false
-            break
-        }
-        divisor++
-    }
-    if(primo && num != 1 && num != 0){
-        soma_primo = soma_primo + num
-    }
 }
+
 console.log(`Soma dos números pares: ${soma_par}`)
 console.log(`Soma dos números primos: ${soma_primo}`)
